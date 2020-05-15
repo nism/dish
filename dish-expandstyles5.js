6f2f3e30b270aa57436861586b636c82079294d5
nism <eitthvad@gmail.com>
2020-05-15 12:59:01 +0900

    Add files via upload

---
+++ dish-expandstyles5.js
@@ -0,0 +1,30 @@
+$(function(){
+  if (document.body.innerHTML.includes('[NOSIDEBAR]')) {
+    sidebarElement = document.getElementsByClassName("l-side");
+    mainContainerElement = document.getElementsByClassName("l-main");
+
+    sidebarElement[0].classList.add("l-nosidebar");
+    mainContainerElement[0].classList.add("l-main-nosidebar");
+
+    document.body.innerHTML = document.body.innerHTML.replace('[NOSIDEBAR]', '');
+  }
+
+  if (document.body.innerHTML.includes('[CANADATOPIMAGE]')) {
+    document.body.innerHTML = document.body.innerHTML.replace('[CANADATOPIMAGE]', '<div class="canada-topimage">');
+    document.body.innerHTML = document.body.innerHTML.replace('[/CANADATOPIMAGE]', '</div>');
+  }
+
+  if (document.body.innerHTML.includes('[CANADAMIDIMAGE]')) {
+    document.body.innerHTML = document.body.innerHTML.replace('[CANADAMIDIMAGE]', '<div class="canada-midimage">');
+    document.body.innerHTML = document.body.innerHTML.replace('[/CANADAMIDIMAGE]', '</div>');
+  }
+
+  if (document.body.innerHTML.includes('[CANADABOTIMAGE]')) {
+    document.body.innerHTML = document.body.innerHTML.replace('[CANADABOTIMAGE]', '<div class="canada-botimage">');
+    document.body.innerHTML = document.body.innerHTML.replace('[/CANADABOTIMAGE]', '</div>');
+  }
+
+  document.getElementsByClassName("av-special-heading-tag")[0].parentNode.classList.add("h3div");
+});
+
+//document.body.innerHTML = document.body.innerHTML.replace('', '');
