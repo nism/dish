ee4602a5956d0e44067d0dba7a045d5d523ac467
nism <eitthvad@gmail.com>
2020-05-15 11:38:57 +0900

    Add files via upload

--- dish-expandstyles.js
+++ dish-expandstyles.js
@@ -1,24 +1,28 @@
-if (document.body.innerHTML.includes('[NOSIDEBAR]')) {
-  sidebarElement = getElementByClassName("l-side") ;
-  mainContainerElement = getElementByClassName("l-main");
+$(function(){
+  if (document.body.innerHTML.includes('[NOSIDEBAR]')) {
+    sidebarElement = document.getElementsByClassName("l-side");
+    mainContainerElement = document.getElementsByClassName("l-main");
 
-  sidebarElement.classList.add("l-nosidebar");
-  mainContainerElement.classList.add("l-main-nosidebar");
-}
+    sidebarElement[0].classList.add("l-nosidebar");
+    mainContainerElement[0].classList.add("l-main-nosidebar");
 
-if (document.body.innerHTML.includes('[CANADATOPIMAGE]')) {
-  document.body.innerHTML = document.body.innerHTML.replace('[CANADATOPIMAGE]', '<div class="canada-topimage">');
-  document.body.innerHTML = document.body.innerHTML.replace('[/CANADATOPIMAGE]', '</div>');
-}
+    document.body.innerHTML = document.body.innerHTML.replace('[NOSIDEBAR]', '');
+  }
 
-if (document.body.innerHTML.includes('[CANADAMIDIMAGE]')) {
-  document.body.innerHTML = document.body.innerHTML.replace('[CANADAMIDIMAGE]', '<div class="canada-midimage">');
-  document.body.innerHTML = document.body.innerHTML.replace('[/CANADAMIDIMAGE]', '</div>');
-}
+  if (document.body.innerHTML.includes('[CANADATOPIMAGE]')) {
+    document.body.innerHTML = document.body.innerHTML.replace('[CANADATOPIMAGE]', '<div class="canada-topimage">');
+    document.body.innerHTML = document.body.innerHTML.replace('[/CANADATOPIMAGE]', '</div>');
+  }
 
-if (document.body.innerHTML.includes('[CANADABOTIMAGE]')) {
-  document.body.innerHTML = document.body.innerHTML.replace('[CANADABOTIMAGE]', '<div class="canada-botimage">');
-  document.body.innerHTML = document.body.innerHTML.replace('[/CANADABOTIMAGE]', '</div>');
-}
+  if (document.body.innerHTML.includes('[CANADAMIDIMAGE]')) {
+    document.body.innerHTML = document.body.innerHTML.replace('[CANADAMIDIMAGE]', '<div class="canada-midimage">');
+    document.body.innerHTML = document.body.innerHTML.replace('[/CANADAMIDIMAGE]', '</div>');
+  }
+
+  if (document.body.innerHTML.includes('[CANADABOTIMAGE]')) {
+    document.body.innerHTML = document.body.innerHTML.replace('[CANADABOTIMAGE]', '<div class="canada-botimage">');
+    document.body.innerHTML = document.body.innerHTML.replace('[/CANADABOTIMAGE]', '</div>');
+  }
+});
 
 //document.body.innerHTML = document.body.innerHTML.replace('', '');
